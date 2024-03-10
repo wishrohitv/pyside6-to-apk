241429fa8f4d14aea3064020ecceea6dfc17d843
Rohit Vishwakarma <126248971+wishrohitv@users.noreply.github.com>
2024-03-10 18:59:07 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git clone https://code.qt.io/pyside/pyside-setup
+cd pyside-setup
+git checkout 6.6.1
+pip install -r requirements.txt
+pip3 install -r tools/cross_compile_android/requirements.txt
+pip3 install pyside6
